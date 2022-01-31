7248a8a830021532a1a8815efa264d7265e040cd
JaretWright <jaret.wright@georgiancollege.ca>
2022-01-31 10:42:05 -0500

    Updated the project to support additional phone number combinations.

--- src/main/java/com/example/w22comp1011gcw4regex/HelloController.java
+++ src/main/java/com/example/w22comp1011gcw4regex/HelloController.java
@@ -1,10 +1,14 @@
 package com.example.w22comp1011gcw4regex;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class HelloController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
 
     @FXML
     private TextField phoneNumTextField;
@@ -12,4 +16,20 @@ public class HelloController {
     @FXML
     private Label resultLabel;
 
+    @FXML
+    private Label replaceAllLabel;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        phoneNumTextField.textProperty().addListener((obs, oldValue, phoneNumber)->{
+            if (phoneNumber.matches("\\(?[2-9][0-9][0-9]\\)?[-\\s.]?[2-9]\\d{2}[-\\s.]?[0-9]{4}"))
+                resultLabel.setText(String.format("%s is a valid phone number",phoneNumber));
+            else
+                resultLabel.setText(String.format("%s is not a valid phone number",phoneNumber));
+
+            //using replaceAll() with our String to remove special characters before the analysis
+            String phoneNumJustNumbers = phoneNumber.replaceAll("[-\\s\\(\\).]","");
+            replaceAllLabel.setText(String.format("Removing special characters: '%s'",phoneNumJustNumbers));
+        });
+    }
 }
