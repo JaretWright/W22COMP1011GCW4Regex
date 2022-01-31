079dd48ddfd8320384e72ff545fac0b45dc4fd0f
JaretWright <jaret.wright@georgiancollege.ca>
2022-01-31 09:47:39 -0500

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.w22comp1011gcw4regex {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.w22comp1011gcw4regex to javafx.fxml;
+    exports com.example.w22comp1011gcw4regex;
+}
