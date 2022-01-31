8fa1f67734ff971f8ae040fe7471c0ccdcd13fbc
JaretWright <jaret.wright@georgiancollege.ca>
2022-01-31 10:07:41 -0500

    Updated the view and controller to support the textfield and label for entering phone information

--- src/main/java/com/example/w22comp1011gcw4regex/HelloController.java
+++ src/main/java/com/example/w22comp1011gcw4regex/HelloController.java
@@ -2,13 +2,14 @@ package com.example.w22comp1011gcw4regex;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private TextField phoneNumTextField;
+
+    @FXML
+    private Label resultLabel;
+
 }
